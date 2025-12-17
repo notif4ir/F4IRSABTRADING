a146476798fd00642d231405a53cb510fe743ed6
notif4ir <filiptrz@interia.pl>
2025-12-17 19:51:39 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 export const ITEMS = [
-  { name: "Ancient Coin", value: 120, defaultGen: "$10/s" },
-  { name: "Crystal Shard", value: 240, defaultGen: "$25/s" },
-  { name: "Golden Token", value: 400, defaultGen: "$2K/s" },
-  { name: "Mystic Scroll", value: 320, defaultGen: "$850/s" },
+  { name: "Gueirro Digitale", value: 300, defaultGen: "$450K/s" },
+  { name: "67", value: 5500, defaultGen: "$7.5M/s" },
+  { name: "Los 67", value: 19500, defaultGen: "$22.5M/s" },
+  { name: "La Grande Combinassion", value: 9000, defaultGen: "$10M/s" },
   { name: "Shadow Dagger", value: 280, defaultGen: "$1.2K/s" },
   { name: "Emerald Gem", value: 200, defaultGen: "$3K/s" },
   { name: "Ruby Gem", value: 260, defaultGen: "$4.5K/s" },
